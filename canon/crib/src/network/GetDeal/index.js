fc50d8146887d789cfe25bca88901b6175346cad
nik frank <nikf@sweetinn.com>
2017-04-21 00:17:57 +0300

    fix deal no dupes

--- canon/crib/src/network/GetDeal/index.js
+++ canon/crib/src/network/GetDeal/index.js
@@ -7,18 +7,19 @@ class GetDeal {
     // action can define number of cards, list of cards already used
 
     const { size, burned } = action.network.payload;
-    
-    // make up random cards
-    const card = { rank: 1, suit: 0 };
+
+    let liveBurned = JSON.parse( JSON.stringify( burned ) );
 
     let cards = [];
     while( cards.length < size ){
       const rank = Math.floor(Math.random()*13);
       const suit = Math.floor(Math.random()*4);
 
-      if ( !burned.filter( c => ((c.rank === rank) && (c.suit === suit)) ).length )
+      if ( !liveBurned.filter( c => ((c.rank === rank) && (c.suit === suit)) ).length ) {
         cards.push({ rank, suit });
-    }
+        liveBurned.push({ rank, suit });
+      }
+      }
     
     this.next({ payload: cards });
   }
