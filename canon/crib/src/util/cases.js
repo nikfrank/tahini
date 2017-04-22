6c690eb25e35d30232041fc85cdc3e4207d37b2f
nik frank <nikf@sweetinn.com>
2017-04-23 01:36:31 +0300

    dibs pts

--- canon/crib/src/util/cases.js
+++ canon/crib/src/util/cases.js
@@ -10,4 +10,16 @@ export default [
     cut: { rank: 2, suit: 3 },
     value: 12,
   },
+  
+  {
+    hand: [
+      { rank: 4, suit: 0 },
+      { rank: 2, suit: 1 },
+      { rank: 6, suit: 2 },
+      { rank: 11, suit: 3 },
+
+    ],
+    cut: { rank: 8, suit: 3 },
+    value: 1,
+  },
 ];
--- canon/crib/src/util/score.js
+++ canon/crib/src/util/score.js
@@ -3,11 +3,14 @@
 export default (hand, cut) => {
   let total = 0;
 
+  const dibsPts = hand.filter( ({ rank, suit }) => ((rank === 11) && (suit === cut.suit)) ).length;
+  
   const cards = hand.concat(cut);
   
   const pairPts = cards.reduce( (p, c, i) =>
     p + cards.slice(i + 1).filter( ({ rank }) => (rank === c.rank) ).length * 2, 0);
 
+  total += dibsPts;
   total += pairPts;
 
   return total;
