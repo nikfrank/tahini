b52925ed75a2dcb842c5f1c2a9bf3cc27839bbac
nik frank <nikbfrank@gmail.com>
2017-07-18 16:04:26 +0300

    mock basics

--- src/network-handlers/simpleHTTP/index.js
+++ src/network-handlers/simpleHTTP/index.js
@@ -1,27 +1,50 @@
 import Fetcher from '../http/';
 
-export default {
-  'get': (url)=>
-    class simpleFetcher {
-      constructor(next, done, err){
-        this.fetcher = new Fetcher(
-          response=> next({ payload: response }),
-          done,
-          err
-        );
+
+const factory = ({ url, method, headers })=>  
+  class simpleFetcher {
+    constructor(next, done, err){
+      this.fetcher = new Fetcher(
+        response=> next({ payload: response }),
+        done,
+        err
+      );
+    }
+
+    handleRequest(action){
+      action.network.url = url;
+
+      if( (typeof action.network.payload === 'object') &&
+          ( method === 'GET' )) {
+        action.network.url +=
+          Object.keys(action.network.payload).reduce((p, c)=>
+            p+''+c+'='+encodeURIComponent(action.network.payload[c])+'&', '?')
+                .slice(0,-1);
       }
 
-      handleRequest(action){
-        action.network.url = url;
+      action.network.method = method;
+      
+      this.fetcher.handleRequest(action);
+    }
+  };
 
-        if(typeof action.network.payload === 'object'){
-          action.network.url +=
-            Object.keys(action.network.payload).reduce((p, c)=>
-              p+''+c+'='+encodeURIComponent(action.network.payload[c])+'&', '?')
-                  .slice(0,-1);
-        }
-        
-        this.fetcher.handleRequest(action);
+
+export default {
+  'get': (url)=> factory({ url, method: 'GET' }),
+  'post': (url)=> factory({ url, method: 'POST' }),
+  'put': (url)=> factory({ url, method: 'PUT' }),
+
+  
+  mock: (response)=>
+    class simpleMock {
+      constructor(next, done, err){
+        this.next = next;
+        this.done = done;
+      }
+
+      handleRequest(){
+        this.next({ payload: response });
+        this.done();
       }
     },
-}
+};
