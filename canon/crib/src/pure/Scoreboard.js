612570fc5a7dac94f048a9d88cbefc1b91aa0fce
nik frank <nikbfrank@gmail.com>
2017-10-02 21:40:44 +0300

    draft score tracks

--- canon/crib/src/pure/Scoreboard.js
+++ canon/crib/src/pure/Scoreboard.js
@@ -3,43 +3,46 @@ import { fromJS } from 'immutable';
 
 const seStyle = {
   position: 'fixed',
-  top: 0,
-  left: 0,
-  width: 1,
-  height: 1,
-  color: 'red'
+  color: 'red',
+  zIndex: 10,
 };
 
+const trackWidth = 2;
+
 const divColor = ({ pts, player })=> ([0,1,2].map( pp=>
-  Math.min( 255, player !== pp ? 0 : pts * 12 )
-);
+  Math.min( 255, player !== pp ? 0 : (100 + pts * 6) )
+) );
 
 const divPerSE = ({ pts, total, player, color=divColor({pts, player}) }) => {
-  if( ((total + pts) % 30) < (total % 30) ){
+
+  if( ((total + pts) % 30) && (((total + pts) % 30) < (total % 30) ) ){
     return [
       divPerSE({
         pts: pts - ( (total + pts) % 30 ),
         total,
         player,
+        color,
       }),
       divPerSE({
         pts: (total + pts) % 30,
         total: (total + pts) - ( (total + pts) % 30 ),
         player,
+        color,
       }),
     ];
   } else {
-    const edgeDim = ['top', 'right', 'bottom', 'left'][ Math.floor(total / 30) ];
-    const runDim = ['left', 'top', 'right', 'bottom'][ Math.floor(total / 30) ];
+    const edgeDim = ['top', 'right', 'bottom', 'left'][ Math.floor(total / 30) % 4];
+    const runDim = ['left', 'top', 'right', 'bottom'][ Math.floor(total / 30) % 4];
     const thinDim = ['height', 'width'][ Math.floor(total / 30) % 2 ];
     const thickDim = ['width', 'height'][ Math.floor(total / 30) % 2 ];
     
     return {
-      [edgeDim]: player,
-      [runDim]: `${(total - (total % 30)) / 0.3}v${thickDim[0]}`,
-      [thinDim]: 1,
-      [thickDim]: `${pts / 0.3}v${thickDim[0]}`,
+      [edgeDim]: player * trackWidth,
+      [runDim]: `calc( ${player*trackWidth}px + ${(total % 30) / 0.3}v${thickDim[0]} )`,
+      [thinDim]: trackWidth,
+      [thickDim]: `calc( ${pts / 0.3}v${thickDim[0]} - ${2*player*trackWidth + 2}px )`,
 
+      border: '1px solid white',
       backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
     };
   }
@@ -54,30 +57,42 @@ export default ({ scoring }) => {
   const lastScore = scoring.last() || fromJS({});
   const lastScorePlayer = ['cp', 'me'][lastScore.get('player')] || null;
 
-  const p1ScoringDivs = scoring
-    .filter( s => s.get('player') === 1)
-    .reduce((p, c)=> ({
+  const scoringDivs = [0, 1].map(player =>
+    scoring
+      .filter( s => s.get('player') === player)
+      .filter( s => s.get('pts') )
+      .reduce((p, c)=> ({
         total: p.total + c.get('pts'),
         divs: p.divs.concat( divPerSE({
           pts: c.get('pts'),
           total: p.total,
-          player:1,
+          player: player,
         }) )
       }), { total: 0, divs: [] }
-    ).divs;
+      ).divs
+  );
   
   return (
     <div>
       {
-        p1ScoringDivs.map( se => (
-          <div style={{ ...seStyle, ...se }}/>
+        scoringDivs[0].map( (se, sei) => (
+          <div key={'0'+sei} style={{ ...seStyle, ...se }}/>
         ) )
       }
-      <p>
-        my pts: {myPts} cp pts: {cpPts}
+      
+      {
+        scoringDivs[1].map( (se, sei) => (
+          <div key={'1'+sei} style={{ ...seStyle, ...se }}/>
+        ) )
+      }
+      <p style={{ textAlign: 'center' }}>
         {
-          lastScorePlayer ?
-          ('  ' + lastScorePlayer +' + '+lastScore.get('pts')) : null
+          lastScorePlayer ? (
+            <span>
+              my pts: {myPts} cp pts: {cpPts}
+              {('  ' + lastScorePlayer +' + '+lastScore.get('pts'))}
+            </span>
+          ): null
         }
       </p>
     </div>
